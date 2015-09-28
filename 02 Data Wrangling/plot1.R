270a42a5217361cacacc8d7b44ee8fe771b4e3e5
Garner Vincent <garnervincent@gmail.om>
2015-09-28 16:41:10 -0500

    Finished Plot #1.

--- 02 Data Wrangling/plot1.R
+++ 02 Data Wrangling/plot1.R
@@ -1,12 +1,14 @@
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COLLEGESTATS where tuitionfees1314 is not null"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+head(df)
 ggplot() + 
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
-  labs(title='Titanic') +
-  labs(x="Age", y=paste("Fare")) +
-  layer(data=nullDf, 
-        mapping=aes(x=as.numeric(as.character(AGE)), y=as.numeric(as.character(FARE)), color=SEX), 
+  facet_grid(as.character(PUBLICPRIVATE~PUBLIC), labeller=label_both) +
+  labs(title='College Stats') +
+  labs(x="Tuition and Fees", y=paste("Student Faculty Ratio")) +
+  layer(data=df, 
+        mapping=aes(x=as.numeric(as.character(TUITIONFEES1314)), y=as.numeric(as.character(STUDENTFACULTYRATIO)), color=PUBLICPRIVATE), 
         stat="identity", 
         stat_params=list(), 
         geom="point",
